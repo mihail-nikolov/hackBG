62ccf4ce07018a1794f76da3109535e4fb59dbfe
mihail-nikolov <mihail.y.nikolov@abv.bg>
2014-10-10 13:18:03 +0300

    new file

---
+++ 132123.py
@@ -0,0 +1,15 @@
+array = ["apple", "bottle", "apple", "little", "little"]
+
+
+def word_count(arr):
+    dictionary = {}
+    print(dictionary)
+    for i in arr:
+        if i in dictionary:
+            dictionary[i] += 1
+        else:
+            dictionary[i] = 1
+
+    return dictionary
+
+print(word_count(array))
