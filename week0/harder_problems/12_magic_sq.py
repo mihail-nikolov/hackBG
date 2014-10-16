30a2b580634fecb390ca72b1f48fe43f32ba7279
mihail-nikolov <mihail.y.nikolov@abv.bg>
2014-10-16 18:05:42 +0300

    modify magic square

--- week0/harder_problems/12_magic_sq.py
+++ week0/harder_problems/12_magic_sq.py
@@ -1,4 +1,6 @@
 square = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
+for row in square:
+    print(row)
 
 
 def check_sub_results(arr):
@@ -8,13 +10,29 @@ def check_sub_results(arr):
             return False
     return n
 
-"""
-def sumColumn(m, column):
-    total = 0
-    for row in range(len(m)):
-        total += m[row][column]
-    return total
-"""
+
+def sum_diag(matrix):
+    n1 = 0
+    n2 = 0
+    result_diags = []
+    for i in range(len(matrix)):
+        n1 += matrix[i][i]
+    result_diags.append(n1)
+    for ind_row in reversed(range(len(matrix))):
+        col = abs(i-3)
+        n2 += matrix[ind_row][col]
+    result_diags.append(n2)
+    return check_sub_results(result_diags)
+
+
+def sum_cols(matrix):
+    result_cols = []
+    for column in range(len(matrix[0])):
+        n = 0
+        for row in matrix:
+            n += row[column]
+        result_cols.append(n)
+    return check_sub_results(result_cols)
 
 
 def sum_rows(matrix):
@@ -28,6 +46,13 @@ def sum_rows(matrix):
     return check_sub_results(result_rows)
 
 
-print(sum_rows(square))
+def magic_square(matrix):
+    diags = sum_diag(matrix)
+    cols = sum_cols(matrix)
+    rows = sum_rows(matrix)
+    if (rows is False) or (cols is False) or (diags is False):
+        return "It is NOT a magic square"
+    elif rows == cols and cols == diags:
+        return "It is a magic square"
 
-#def magic_square(arr):
+print(magic_square(square))
